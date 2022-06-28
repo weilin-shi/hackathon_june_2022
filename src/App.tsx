09ce0bb20be53b773c3632cfc35e6f5c9e5e93cd
Weilin Shi <934587911@qq.com>
2022-06-28 10:42:30 +0800

    video preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,50 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useRef } from "react";
+
+const src =
+  "http://a9f15b20f9fca46e2b655ffd55f6be3d-914171737.us-west-2.elb.amazonaws.com:7700/thumbnail?token=aHR0cHM6Ly9jZG4ubGlmdG9mZi5pby9jdXN0b21lcnMvNjA5NmZjOTc1YS92aWRlb3MvbW9iaWxlL2EwOTllMDkyZDUwZDIzNDBhMWY3Lm1wNA==&path=thumbnail_temp/thumbnail3708990603/aHR0cHM6Ly9jZG4ubGlmdG9mZi5pby9jdXN0b21lcnMvNjA5NmZjOTc1YS92aWRlb3MvbW9iaWxlL2EwOTllMDkyZDUwZDIzNDBhMWY3Lm1wNA==_5.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tapToPlay, setTapToPlay] = useState(true);
+  const [videoVis, setVideoVis] = useState(true);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="mx-auto w-96">
+      <div className="my-10 flex">
+        <input
+          type="text"
+          className="flex-1 text-slate-600 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 hover:ring-slate-400 focus:ring-slate-400 outline-none"
+        />
+        <button className="button ml-4">Generate</button>
+      </div>
+      <div className="grid grid-cols-3 grid-rows-3">
+        <img src={src} className="endcard" alt="logo" />
+        {videoVis && (
+          <video
+            ref={videoRef}
+            className="main-video"
+            src="https://cdn-lb.vungle.com/zen/VungleBrandV2Portrait-720x1280-Q2.mp4"
+            muted
+            loop={false}
+            onEnded={() => {
+              setVideoVis(false);
+            }}
+          ></video>
+        )}
+        {tapToPlay && (
+          <div
+            className="tap-to-play w-full h-full relative flex items-center justify-center backdrop-opacity-80 bg-slate-50/80"
+            onClick={() => {
+              setTapToPlay(false);
+              videoRef.current!.play();
+            }}
           >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+            <div className="text-6xl font-bold">Tap to Play</div>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
