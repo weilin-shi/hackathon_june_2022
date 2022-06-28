6a854e922b98a96a32494a06662630f797f05b38
Weilin Shi <934587911@qq.com>
2022-06-28 22:43:41 +0800

    mute option

--- src/App.tsx
+++ src/App.tsx
@@ -8,70 +8,99 @@ const imageURLBase =
   "http://a9f15b20f9fca46e2b655ffd55f6be3d-914171737.us-west-2.elb.amazonaws.com:7700/thumbnail?path=";
 
 function App() {
-  const [imgSRC, setImgSrc] = useState("");
+  const [images, setImages] = useState<string[]>([]);
   const [videoSRC, setVideoSrc] = useState("");
+  const [muted, setMuted] = useState(false);
   const [input, setInput] = useState("");
   const [tapToPlay, setTapToPlay] = useState(true);
   const [videoVis, setVideoVis] = useState(true);
   const videoRef = useRef<HTMLVideoElement>(null);
+  let imgSrc = "";
+  if (images.length > 0) {
+    imgSrc = images[images.length - 1];
+  }
 
   return (
-    <div className="mx-auto w-96">
-      <div className="my-10 flex">
-        <input
-          type="text"
-          className="flex-1 text-slate-600 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 hover:ring-slate-400 focus:ring-slate-400 outline-none"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Video Link"
-        />
-        <button
-          className="button ml-4"
-          disabled={input.trim() === ""}
-          onClick={() => {
-            setVideoSrc(input);
-            setInput("");
-            const uri = `${baseURL}${btoa(input)}${tailURL}`;
-            axios
-              .get(uri)
-              .then((result) => {
-                const imgLinks = result.data.Thumbnails as string[];
-                setImgSrc(imageURLBase + imgLinks[imgLinks.length - 1]);
-              })
-              .catch(console.error);
-          }}
-        >
-          Generate
-        </button>
-      </div>
-      {videoSRC && (
-        <div className="grid grid-cols-3 grid-rows-3">
-          <img src={imgSRC} className="endcard" alt="logo" />
-          {videoVis && (
-            <video
-              ref={videoRef}
-              className="main-video"
-              src={videoSRC}
-              muted
-              loop={false}
-              onEnded={() => {
-                setVideoVis(false);
-              }}
-            ></video>
-          )}
-          {tapToPlay && (
-            <div
-              className="tap-to-play w-full h-full relative flex items-center justify-center backdrop-opacity-80 bg-slate-50/80"
-              onClick={() => {
-                setTapToPlay(false);
-                videoRef.current!.play();
-              }}
-            >
-              <div className="text-6xl font-bold">Tap to Play</div>
-            </div>
-          )}
+    <div className="flex justify-center">
+      <div className="w-96">
+        <div className="my-10 flex">
+          <input
+            type="text"
+            className="flex-1 text-slate-600 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 hover:ring-slate-400 focus:ring-slate-400 outline-none"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Video Link"
+          />
+          <button
+            className="button ml-4"
+            disabled={input.trim() === ""}
+            onClick={() => {
+              setVideoSrc(input);
+              setInput("");
+              const uri = `${baseURL}${btoa(input)}${tailURL}`;
+              axios
+                .get(uri)
+                .then((result) => {
+                  const imgLinks = result.data.Thumbnails as string[];
+                  // setImgSrc(imageURLBase + imgLinks[imgLinks.length - 1]);
+                  setImages(imgLinks.map((link) => imageURLBase + link));
+                })
+                .catch(console.error);
+            }}
+          >
+            Generate
+          </button>
         </div>
-      )}
+        {videoSRC && (
+          <div className="grid grid-cols-3 grid-rows-3">
+            <img src={imgSrc} className="endcard" alt="logo" />
+            {videoVis && (
+              <video
+                ref={videoRef}
+                className="main-video"
+                src={videoSRC}
+                muted={muted}
+                loop={false}
+                onEnded={() => {
+                  setVideoVis(false);
+                }}
+              ></video>
+            )}
+            {tapToPlay && (
+              <div
+                className="tap-to-play w-full h-full relative flex items-center justify-center backdrop-opacity-80 bg-slate-50/80"
+                onClick={() => {
+                  setTapToPlay(false);
+                  videoRef.current!.play();
+                }}
+              >
+                <div className="text-6xl font-bold">Tap to Play</div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      <div className="w-96 ml-10">
+        <div className="mt-20 p-2 rounded-lg bg-slate-50">
+          <label className="cursor-pointer">
+            <input
+              type="checkbox"
+              className="mr-2"
+              checked={muted}
+              onChange={(e) => setMuted(e.target.checked)}
+            />
+            {`${muted ? "Unmute" : "Mute"} Video`}
+          </label>
+            <h3 className="my-4">Image Preview:</h3>
+          <div className="flex">
+            {images.map(src => (
+              <div key={src} className="mr-2 mb-2 ring-1">
+                <img src={src} alt="image preview" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/hackathon_june_2022/"
+  plugins: [react()]
 })
